e9815af418c591c29dfc2fd9ac3e31e846102380
apple <apple@apples-MacBook-Pro.local>
2024-02-19 16:41:53 +0500

    chore(): demo openapi code generator typescript

---
+++ src/generated-sources/openapi/models/index.ts
@@ -0,0 +1,16 @@
+/* tslint:disable */
+/* eslint-disable */
+export * from './APIError';
+export * from './APIKeyValidationResponse';
+export * from './AddUserToTeamRequest';
+export * from './CreateAPIKeyRequest';
+export * from './CreateAPIKeyResponse';
+export * from './CreateTeamRequest';
+export * from './LoginRequest';
+export * from './RegisterationRequest';
+export * from './RegisterationResponse';
+export * from './Team';
+export * from './TeamList';
+export * from './TeamUser';
+export * from './TokenResponse';
+export * from './User';
